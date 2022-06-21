9fc9b8394bc8d14b58aaf623b1af8b5ad3804b7a
ARCTURUS <1638330246@qq.com>
2022-06-21 19:21:51 +0800

    feat: 初始化配置文件命令

---
+++ src/bin/init.ts
@@ -0,0 +1,25 @@
+const chalk = require('chalk');
+const path = require('path');
+const fs = require('fs');
+
+const targetFile = path.resolve('./config.json');
+
+const json = `{
+  "dir": "./assets",
+  "save_dir": "./weather-icon",
+  "default_type": "qweather",
+  "default_size": 18
+}`;
+
+if (fs.existsSync(targetFile)) {
+  console.error(chalk.red(`config.json already exists.`));
+} else {
+  fs.writeFile(targetFile, json, (err: Error) => {
+    if (err) throw err;
+
+    console.clear();
+    console.log(chalk.green(`🎉 config.json created successfully.`));
+  });
+}
+
+export {};
