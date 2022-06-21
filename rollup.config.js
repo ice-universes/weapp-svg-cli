c16e116da26bbba43b0054d5ac5656d6ae812912
ARCTURUS <1638330246@qq.com>
2022-06-21 21:20:36 +0800

    feat: 添加打包入口

--- rollup.config.js
+++ rollup.config.js
@@ -20,12 +20,37 @@ const plugins = [typescript(), resolve(), commonjs(), terser(), cleanup()];
 export default defineConfig([
   {
     plugins: plugins,
-    input: ['src/bin/help.ts', 'src/bin/init.ts'],
+    input: ['src/bin/help.ts', 'src/bin/init.ts', 'src/bin/generate.ts'],
     output: {
       dir: 'dist/bin',
       format: 'es',
       entryFileNames: '[name].js',
       banner: '#!/usr/bin/env node',
+    },
+  },
+  {
+    plugins: plugins,
+    input: [
+      'src/libs/generate.ts',
+      'src/libs/getConfig.ts',
+      'src/libs/getSvg.ts',
+      'src/libs/getTemplate.ts',
+      'src/libs/replace.ts',
+      'src/libs/utils.ts',
+    ],
+    output: {
+      dir: 'dist/libs',
+      format: 'es',
+      entryFileNames: '[name].js',
+    },
+  },
+  {
+    plugins: plugins,
+    input: ['src/template/weapp.ts'],
+    output: {
+      dir: 'dist/template',
+      format: 'es',
+      entryFileNames: '[name].js',
     },
   },
 ]);
